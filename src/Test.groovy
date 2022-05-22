7e8788df872d7dd3a9a1dc9e040b85da73b8c9b3
hujia <hujia@bilibili.com>
2022-05-22 15:51:26 +0800

    Initial commit

---
+++ src/Test.groovy
@@ -0,0 +1,22 @@
+//class Test{
+//    static void main(String[] args) {
+//        def range = 5..10
+//        println(5 in range)
+//        println(11 in range)
+//        println(range.get(2))
+//
+//        3.times {
+//            println(it)
+//        }
+//    }
+//}
+def range = 5..10
+println(5 in range)
+println(11 in range)
+println(range.get(2))
+
+3.times {
+    println(it)
+}
+
+
